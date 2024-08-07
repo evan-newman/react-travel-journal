25c2fcd6677c01faa036c115377bd04e85426832
evan-newman <newmanrevan@gmail.com>
2024-08-07 10:00:57 -0700

    Set up project for travel journal. Removed boiler plate not being used. Added in Header for the site with styling

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import './Header.css'
+
+function Header() {
+    return (
+        <header>
+            <i className="fa fa-globe"></i>
+            <h2 className="title">my travel journal.</h2>
+        </header>
+    )
+}
+
+export default Header;
